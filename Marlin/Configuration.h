60a74de9f7170a97ea767ff7c07150abec3c4167
messschuh <messschuh@gmx.de>
2022-06-06 18:10:41 +0200

    tune conf

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -743,11 +743,11 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 200, 180, 40, 80 }
+#define DEFAULT_MAX_FEEDRATE          { 200, 180, 15, 40 }
 
-//#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
+#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
-  #define MAX_FEEDRATE_EDIT_VALUES    { 600, 600, 10, 50 } // ...or, set your own edit limits
+  #define MAX_FEEDRATE_EDIT_VALUES    { 200, 180, 30, 50 } // ...or, set your own edit limits
 #endif
 
 /**
